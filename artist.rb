97e1abbd6cf9d2160ee86249c862cf63dcb9d550
larryvsdata <larryvsdata@gmail.com>
2018-10-27 08:08:02 -0700

    Done.

--- artist.rb
+++ artist.rb
@@ -1,3 +1,12 @@
 class Artist < ActiveRecord::Base
+  attr_accessor :name , :genre , :age , :hometown
+
+  def initialize(name , genre ,age ,hometown)
+    @name = name
+    @genre = genre
+    @age = age
+    @hometown = hometown
+
+  end
 
 end
